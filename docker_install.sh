1c5a67ad2c9ecb2907efb2e56a276f0df07f1f75
Shishir Waglay <138477319+waglay@users.noreply.github.com>
2024-05-18 16:12:57 +0545

    removed sudo from docker_install for Jenkins use

--- docker_install.sh
+++ docker_install.sh
@@ -5,14 +5,14 @@ if [ $name -eq 0 ]; then
 echo "Docker Already Installed"
 else
 echo "Installing Docker"
-sudo yum install docker -y
-sudo systemctl enable docker
-sudo systemctl restart docker
-sudo usermod -aG docker ec2-user
+yum install docker -y
+systemctl enable docker
+systemctl restart docker
+usermod -aG docker ec2-user,root
 echo "Docker Installed"
 echo "Setting up docker-compose"
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "Installed Docker Compose"
 fi
