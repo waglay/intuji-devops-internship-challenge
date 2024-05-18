5e59e02273b023a5cbf0d796793536cde8585444
Shishir Waglay <138477319+waglay@users.noreply.github.com>
2024-05-18 15:36:50 +0545

    Adding condition and docker-compose setup

--- docker_install.sh
+++ docker_install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+docker -help
+name=`echo $?`
+if [ $name -eq 0 ]; then
+echo "Docker Already Installed"
+else
+echo "Installing Docker"
 sudo yum install docker -y
 sudo systemctl enable docker
 sudo systemctl restart docker
 sudo usermod -aG docker ec2-user
+echo "Docker Installed"
+echo "Setting up docker-compose"
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "Installed Docker Compose"
+fi
