6368ba3a0e459c266585a24a3f4003b5ef54520a
Shishir Waglay <138477319+waglay@users.noreply.github.com>
2024-05-18 15:25:18 +0545

    Create Jenkins_Setup.sh

---
+++ Jenkins_Setup.sh
@@ -0,0 +1,6 @@
+sudo yum install java-17-amazon-corretto
+sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins
+systemctl enable jenkins
+systemctl start jenkins
