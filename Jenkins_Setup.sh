4d6683a379c3231313c03f99a72bdd3a3757d30e
Shishir Waglay <138477319+waglay@users.noreply.github.com>
2024-05-18 16:02:08 +0545

    Update Jenkins_Setup.sh to install git

--- Jenkins_Setup.sh
+++ Jenkins_Setup.sh
@@ -1,6 +1,7 @@
-sudo yum install java-17-amazon-corretto
+sudo yum install java-17-amazon-corretto -y
+sudo yum install git -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum install jenkins
+sudo yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
