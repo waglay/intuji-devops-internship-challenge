fa24241367a97f4c9b71254274e2bf2d178c4ce9
Shishir Waglay <138477319+waglay@users.noreply.github.com>
2024-05-18 16:30:59 +0545

    Update docker_install.sh condition removed

--- docker_install.sh
+++ docker_install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-docker -help
-name=`echo $?`
-if [ $name -eq 0 ]; then
-echo "Docker Already Installed"
-else
 echo "Installing Docker"
 sudo yum install docker -y
 sudo systemctl enable docker
@@ -15,4 +10,3 @@ sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "Installed Docker Compose"
-fi
